fc2e7bfc9b85a0a2f596b4baf0c307248794c059
JiHaD SeR <85656190+J-I-H-A-D@users.noreply.github.com>
2021-08-02 08:11:49 +0530

    Auto Bio Updated
    Gooys Auto Bio Bug Set Akki 😼 ElsaMwol V2 Comming Soon...
    Update Cheytho Any Error Issue Join Now My Official WhatsApp Group https://chat.whatsapp.com/G8RHcba0E5x3L1d9uICyK0 Not Allowed Fake Numbers and Other Country Number Only Allowed +91

--- bot.js
+++ bot.js
@@ -178,7 +178,7 @@ async function whatsAsena () {
                 var ov_time = new Date().toLocaleString('HI', { timeZone: 'Asia/Kolkata' }).split(' ')[1]
                 const get_localized_date = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
                 var utch = new Date().toLocaleDateString(config.LANG, get_localized_date)
-                const biography = '📅 ' + utch + '\n⌚ ' + ov_time + '\n\n🐺 WhatsAsena'
+                const biography = '📅 ' + utch + '\n⌚ ' + ov_time + '\n\n🐺 ElsaMwol'
                 await conn.setStatus(biography)
             }
             else if (conn.user.jid.startsWith('62')) { // Indonesia
