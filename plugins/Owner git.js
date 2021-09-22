255db74ec3190032ccb93f04cf415cfc3d8a1f86
AJNAS-K <91139423+AJNAS-SK@users.noreply.github.com>
2021-09-22 08:53:39 +0530

    Update Owner git.js

--- plugins/Owner git.js
+++ plugins/Owner git.js
@@ -5,7 +5,7 @@ const axios = require('axios');
 const Language = require('../language');
 const Lang = Language.getString('wallpaper');
 
-Asena.addCommand({pattern: 'git', fromMe: false, desc: Lang.WP}, (async (message, match) => {
+Asena.addCommand({pattern: 'git', fromMe: false, desc: Lang.GL}, (async (message, match) => {
 
     var r_text = new Array ();
     
