8417aec898aabaa00fb32b9b91b7e24969349c56
AJNAS-K <91139423+AJNAS-SK@users.noreply.github.com>
2021-10-06 06:24:55 +0530

    Create Owner git.js

---
+++ plugins/Owner git.js
@@ -0,0 +1,45 @@
+const Asena = require('../events');
+
+const {MessageType, MessageOptions, Mimetype} = require('@adiwajshing/baileys');
+
+const axios = require('axios');
+
+const Language = require('../language');
+
+const Lang = Language.getString('wallpaper');
+
+Asena.addCommand({pattern: 'git', fromMe: false, desc: Lang.WP}, (async (message, match) => {
+
+    var r_text = new Array ();
+
+    
+
+    
+
+   
+
+  r_text[0] = "https://telegra.ph/file/285a11813cfd41626bce8.jpg";
+
+    
+
+    
+
+    var i = Math.floor(1*Math.random())
+
+    var respoimage = await axios.get(`${r_text[i]}`, { responseType: 'arraybuffer' })
+
+    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: `*Creater AJNAS SK*
+
+*Best Bgm Bot 🕊️*
+
+*Owner number wa.me/917736703116*
+
+*githublink       https://github.com/AJNAS SK/ElsaMwol*
+
+*audio commads    https://github.com/AJNAS SK/ElsaMwol/tree/master/uploads*
+
+*sticker commads  https://github.com/AJNAS SK/ElsaMwol/tree/master/stickers*
+
+`}) 
+
+}));
